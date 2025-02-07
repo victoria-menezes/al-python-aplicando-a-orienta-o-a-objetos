a60ba7a2d013fb90198e07c014eca37edd912d0d
victoria-menezes <victoria.c.menezes@gmail.com>
2025-02-07 15:07:09 -0300

    Definit __init__, __str__ e listar_restaurantes de Restaurante

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,16 +1,30 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    # Variáveis criadas fora do __init__ são compartilhadas por todos os objetos da mesma classe
+    restaurantes = []
+    
+    # Método construtor: sempre que criarmos uma instância, esse método será chamado
+    # self = se refere à instância ativa, e não qualquer outra. Não precisa ser chamado de self, pode ser 'this' ou qualquer outra variavel. Só precisa ser o primeiro argumento.
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self) # Adicionar o objeto criado à lista restaurantes
+    
+    # Definir como esse objeto será exibido em texto (por exemplo, em print()s)
+    def __str__(self):
+        return '{} - {} | {}'.format(self.nome, self.categoria, 'Ativo' if self.ativo else 'Inativo')
+    
+    def listar_restaurantes():
+        for r in restaurantes:
+            print(r)
 
 # Um objeto = uma instancia de uma classe
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
-
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-# para dar print nos atributos de um objeto, se usa vars(OBJETO)
-print (vars(restaurantes))
+# para dar print nos atributos de um objeto cuja classe nao tem um __str__, se usa vars(OBJETO)
+# print (vars(restaurante_praca))
+
+Restaurante.listar_restaurantes()
